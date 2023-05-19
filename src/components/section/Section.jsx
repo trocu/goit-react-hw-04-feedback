16d12c9e56eca36a0dc836b807b238f20eb44098
Mariusz Trocewicz <throcu@gmail.com>
2023-05-20 00:46:12 +0200

    Section component style changed

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -4,7 +4,7 @@ import css from "./Section.module.css";
 export const Section = ({ title, children }) => {
   return (
     <section className={css.section}>
-      <h1>{title}</h1>
+      <p className={css.title}>{title}</p>
       {children}
     </section>
   );
